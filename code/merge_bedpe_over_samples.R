8013dc162a64f6a98537d61361408c4e6b788262
Ludo Pagie <l.pagie@nki.nl>
2019-02-08 09:21:22 +0100

    style mergeBedpeSmpls; rm comments, etc
    I removed some old production code, some comments

--- code/merge_bedpe_over_samples.R
+++ code/merge_bedpe_over_samples.R
@@ -26,43 +26,21 @@
 # VERSIONS:
 
 
-# setClass("snakemake", representation(output="list", input = "character"))
-# snakemake <- new("snakemake"
-# 		     , output = structure(list(log = "/tmp/tt.log", bedpe = "/tmp/tt.bedpe"), .Names = c("log", 
-# 													 "bedpe"))
-# 		         , input = c("iPCR_SuRE42_2_B1_T01/07_bedpe_BC/equal/iPCR_SuRE42_2_B1_T01_3.bedpe.gz", 
-# 				     "iPCR_SuRE42_2_B1_T02/07_bedpe_BC/equal/iPCR_SuRE42_2_B1_T02_3.bedpe.gz", 
-# 				     "iPCR_SuRE42_2_B1_T03/07_bedpe_BC/equal/iPCR_SuRE42_2_B1_T03_3.bedpe.gz", 
-# 				     "iPCR_SuRE42_2_B1_T04/07_bedpe_BC/equal/iPCR_SuRE42_2_B1_T04_3.bedpe.gz", 
-# 				     "iPCR_SuRE42_2_B1_T05/07_bedpe_BC/equal/iPCR_SuRE42_2_B1_T05_3.bedpe.gz", 
-# 				     "iPCR_SuRE42_2_B1_T06/07_bedpe_BC/equal/iPCR_SuRE42_2_B1_T06_3.bedpe.gz", 
-# 				     "iPCR_SuRE42_2_B1_T07/07_bedpe_BC/equal/iPCR_SuRE42_2_B1_T07_3.bedpe.gz", 
-# 				     "iPCR_SuRE42_2_B1_T08/07_bedpe_BC/equal/iPCR_SuRE42_2_B1_T08_3.bedpe.gz", 
-# 				     "iPCR_SuRE42_2_B1_T09/07_bedpe_BC/equal/iPCR_SuRE42_2_B1_T09_3.bedpe.gz", 
-# 				     "iPCR_SuRE42_2_B1_T10/07_bedpe_BC/equal/iPCR_SuRE42_2_B1_T10_3.bedpe.gz", 
-# 				     "iPCR_SuRE42_2_B1_T11/07_bedpe_BC/equal/iPCR_SuRE42_2_B1_T11_3.bedpe.gz", 
-# 				     "iPCR_SuRE42_2_B1_T12/07_bedpe_BC/equal/iPCR_SuRE42_2_B1_T12_3.bedpe.gz", 
-# 				     "iPCR_SuRE42_2_B1_T13/07_bedpe_BC/equal/iPCR_SuRE42_2_B1_T13_3.bedpe.gz"
-# 				     )
-# 		     )
-
 library(argparse)
 library(data.table)
 
 setDTthreads(threads = 4)
 
 # argument parsing
-
 args <- commandArgs(trailingOnly=TRUE)
 output.bedpe <- args[1]
 output.log   <- args[2]
 input        <- tail(args, -2)
 
+# setup logging function
 logout <- NULL
-
 log <- function(msg="") {
   if (is.null(logout)) {
-#    logout <<- file(description = snakemake@output[["log"]], open = "wt")
     logout <<- file(description = output.log, open = "wt")
     .Last <- function() {
       close(logout)
@@ -74,7 +52,6 @@ log <- function(msg="") {
 
 
 bedpe_fnames <- input
-# bedpe_fnames <- snakemake@input
 log(c("feature-merge-bedpe-R: merging the following bedpe files:",bedpe_fnames)) 
 bedpe <- rbindlist(lapply(bedpe_fnames, fread, nrows=-100, nThread=4))
 log(c("feature-merge-bedpe-R: done importing, start grouping"))
@@ -82,7 +59,6 @@ bedpe <- bedpe[,.(chrom, start=max(start), end=min(end), count=.N, strand, SNP_A
                    ][order(BC,-count), .SD[1,], by=BC]
 log(c("feature-merge-bedpe-R: done grouping, start export"))
 fwrite(x=bedpe, file=output.bedpe, quote=FALSE, sep="\t")
-# fwrite(x=bedpe, file=snakemake@output[['bedpe']], quote=FALSE, sep="\t",nThread=4)
 log("feature-merge-bedpe-R: done export")
 
 log("feature-merge-bedpe-R: Session Info:")
