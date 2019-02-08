152476c07b26294f28c5881271fb4aff1d7989f3
Ludo Pagie <l.pagie@nki.nl>
2019-02-08 09:31:07 +0100

    feature; new script: merge iPCR, cDNA
    script: code/merge_iPCR_cDNA.R
    new rules in SuRE-snakemake
    
    The script is a simple R script which reads the iPCR bedpe file and
    merges it with the counts in all cDNA trimmed-table-count files
    
    The rules in SuRE-snakemake are a straightforward rule
    ('merge_ipcr_cdna') which calls the R script with proper arguments.
    In addition a new target rule is created ('merged_ipcr_cdna').

---
+++ code/merge_iPCR_cDNA.R
@@ -0,0 +1,108 @@
+# AUTHOR / DATE
+#   Ludo Pagie; February 7, 2019; merge_iPCR_cDNA.R
+
+# INTRO / BACKGROUND
+#   Take a bedpe-like and merge it with the counts of a set of inputcDNA coutn
+#   data files. The merge is done on barcode sequence in both data files. The
+#   elements of the input bedpe (ie the SuREfragments) are all retained whereas
+#   the cDNA counts are discarded if no corresponding barcode is present in the
+#   SuRE fragments.
+
+# USAGE / INPUT / ARGUMENTS / OUTPUT
+# USAGE:
+#   required:
+#     Arguments should be given in the following order:
+#     - output bedpe filename
+#     - output log filename
+#     - input bedpe filename
+#     - remaining args are input cdna count filenames
+#   optional:
+
+# VERSIONS:
+
+
+library(argparse)
+library(data.table)
+library(R.utils)
+# set max number of threads to be used by any data.table function:
+setDTthreads(threads=4)
+
+if(0) {
+# argument parsing
+args <- commandArgs(trailingOnly=TRUE)
+output.bedpe <- args[1]
+output.log   <- args[2]
+input.bedpe  <- args[3]
+input.cdna   <- tail(args, -3)
+
+# setup function for logging progress
+logout <- NULL
+log <- function(msg="") {
+  if (is.null(logout)) {
+    logout <<- file(description = output.log, open = "wt")
+    .Last <- function() {
+      close(logout)
+    }
+  }
+  write(x = msg, file = logout)
+  flush(logout)
+}
+}
+
+# prev.time <- cur.time <- start.time <- Sys.time()
+# cat("starting, time is", start.time)
+
+# fns <- 
+#   c(
+#     "/DATA/usr/ludo/projects/LP140430_SureSeq_JvArensbergen/analyses/LP170216_SuRE42/SuRE-pipelineOutput/cDNA/SuRE42_B1_T1/SuRE42_B1_T1_trimmed_table.txt.gz",
+#     "/DATA/usr/ludo/projects/LP140430_SureSeq_JvArensbergen/analyses/LP170216_SuRE42/SuRE-pipelineOutput/cDNA/SuRE42_B2_T1/SuRE42_B2_T1_trimmed_table.txt.gz",
+#     "/DATA/usr/ludo/projects/LP140430_SureSeq_JvArensbergen/analyses/LP170216_SuRE42/SuRE-pipelineOutput/cDNA/SuRE42_B3_T1/SuRE42_B3_T1_trimmed_table.txt.gz",
+#     "/DATA/usr/ludo/projects/LP140430_SureSeq_JvArensbergen/analyses/LP170216_SuRE42/SuRE-pipelineOutput/cDNA/SuRE42_HEPG2_B1_T1/SuRE42_HEPG2_B1_T1_trimmed_table.txt.gz",
+#     "/DATA/usr/ludo/projects/LP140430_SureSeq_JvArensbergen/analyses/LP170216_SuRE42/SuRE-pipelineOutput/cDNA/SuRE42_HEPG2_B2_T1/SuRE42_HEPG2_B2_T1_trimmed_table.txt.gz"
+# 
+#     )
+# dt <- lapply(fns, fread, header=F, nrow=-100000, nThread=1, col.names=c('count','BC'),key='BC')
+# names(dt) <- sub('-T1_trimmed_table.txt.gz','',basename(fns))
+# for(i in seq_along(dt)){colnames(dt[[i]])[1]=names(dt)[i]}
+
+# cat("finished reading cDNA, time is", cur.time <- Sys.time())
+# cat("time elapsed in this step is",cur.time-prev.time); prev.time <- cur.time
+# cat("time elapsed since start is",cur.time-start.time); prev.time <- cur.time
+
+# dt.merged <- Reduce(function(...) merge(..., all=T), yy)
+
+# bedpe.fn <- '/DATA/usr/ludo/projects/LP140430_SureSeq_JvArensbergen/analyses/LP181220_SuRE42-45-INDEL/SuRE42-1_pipelineOutput/iPCR/samples_merged/08_bedpe_merged/equal/5.bedpe.gz'
+# import bedpe data
+bedpe <- fread(input.bedpe, nrow=-10,key='BC')
+
+# cat("finished reading bedpe, time is", cur.time <- Sys.time())
+# cat("time elapsed in this step is",cur.time-prev.time); prev.time <- cur.time
+# cat("time elapsed since start is",cur.time-start.time); prev.time <- cur.time
+
+# import cdna data and merge with bedpe data
+for (f in input.cdna)
+  bedpe <- merge(bedpe, fread(f, header=F, nrow=-100000, nThread=1, col.names=c(sub('-T1_trimmed_table.txt.gz','',basename(f)),'BC'),key='BC'), all.x=TRUE)
+# set all counts not observed in cDNA data to 0
+bedpe[is.na(bedpe)] <- 0
+
+# cat("finished merging cDNA, time is", cur.time <- Sys.time())
+# cat("time elapsed in this step is",cur.time-prev.time); prev.time <- cur.time
+# cat("time elapsed since start is",cur.time-start.time); prev.time <- cur.time
+
+# export bedpe data to file
+if(endsWith(output.bedpe, ".gz")) {
+  fname <- sub(".gz$","",output.bedpe)
+  fwrite(bedpe, fname, sep="\t", quote=FALSE)
+  gzip(fname, destfilename=out.bedpe)
+  unlink(fname)
+}
+else {
+  fwrite(bedpe, output.bedpe, sep="\t", quote=FALSE)
+}
+
+# cat("finished writing and compressing output, time is", cur.time <- Sys.time())
+# cat("time elapsed in this step is",cur.time-prev.time); prev.time <- cur.time
+# cat("time elapsed since start is",cur.time-start.time); prev.time <- cur.time
+
+quit(save='no')
+
